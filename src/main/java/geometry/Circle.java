49283637e58de80fa8e7d3710bc7822a087a8082
AleksBortsov <bortsov.nuos@gmail.com>
2023-01-24 16:30:23 +0200

    Circle test

--- src/main/java/geometry/Circle.java
+++ src/main/java/geometry/Circle.java
@@ -1,2 +1,28 @@
-package geometry;public class Circle {
+package geometry;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Circle {
+    private double centerCircleX;
+    private double centerCircleY;
+    private double radiusX;
+    private double radiusY;
+
+    private double pointX;
+    private double pointY;
+
+    public double radiusOfCircle(double radiusX, double centerCircleX, double radiusY, double centerCircleY) {
+        return Math.hypot(radiusX - centerCircleX, radiusY - centerCircleY);
+    }
+
+    public double distanceFromCentreToPoint(double pointX, double centerCircleX, double pointY, double centerCircleY) {
+        return Math.hypot(pointX - centerCircleX, pointY - centerCircleY);
+    }
+
 }
